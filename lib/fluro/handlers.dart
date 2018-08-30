c8e06359c77dde60addbc3ae72b38d7de9f350da
kekland <kk.erzhan@gmail.com>
2018-08-30 20:59:59 +0600

    Fluro config setup

---
+++ lib/fluro/handlers.dart
@@ -0,0 +1,32 @@
+import 'package:dem_al/onboarding_page/onboarding_page.dart';
+import 'package:dem_al/status_page/status_page.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static String root = '/';
+  static String onboarding = '/onboarding';
+
+  static var rootHandler = new Handler(
+    type: HandlerType.route,
+    handlerFunc: (context, args) {
+      return StatusPage();
+    },
+  );
+
+  static var onboardingHandler = new Handler(
+    type: HandlerType.route,
+    handlerFunc: (context, args) {
+      return OnboardingPage();
+    }
+  );
+
+  static void configureRoutes(Router router) {
+    router.notFoundHandler = new Handler(
+        handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+      print("Route was not found :/");
+    });
+    router.define(root, handler: rootHandler);
+    router.define(onboarding, handler: onboardingHandler);
+  }
+}
